653c371a13af4de6fa357b788a1e5eaa0af956fd
Bruno Oliveira <bruno@abstractj.org>
2013-07-24 17:23:41 -0300

    Fixes indentation issues and avoid to fetch files when the users just want to stop the script

--- servers/activemqtest/server.sh
+++ servers/activemqtest/server.sh
@@ -3,8 +3,6 @@
 BASE_DIR=`pwd`/$(dirname $0)
 ACTIVEMQ_VERSION='apache-activemq-5.8.0'
 DOWNLOAD_URL="http://www.us.apache.org/dist/activemq/apache-activemq/5.8.0/$ACTIVEMQ_VERSION-bin.tar.gz"
-wget -c $DOWNLOAD_URL -P $BASE_DIR/
-
 ACTIVEMQ_PATH="$BASE_DIR/$ACTIVEMQ_VERSION"
 export ACTIVEMQ_DATA="$ACTIVEMQ_VERSION/data"
 
@@ -18,14 +16,18 @@ if [ "$1" == "stop" ]; then
 fi
 
 if [ -d "$ACTIVEMQ_PATH" ]; then
-  rm -rf $ACTIVEMQ_PATH/
+    rm -rf $ACTIVEMQ_PATH/
+fi
+
+if [ ! -f "$ACTIVEMQ_PATH-bin.tar.gz" ]; then
+    wget -c $DOWNLOAD_URL -P $BASE_DIR/
 fi
 
 if [ -f "$ACTIVEMQ_PATH-bin.tar.gz" ]; then
-  tar xzvf $ACTIVEMQ_PATH-bin.tar.gz 
-  tar xzvf $BASE_DIR/conf.tar.gz -C $ACTIVEMQ_VERSION/
+    tar xzvf $ACTIVEMQ_PATH-bin.tar.gz 
+    tar xzvf $BASE_DIR/conf.tar.gz -C $ACTIVEMQ_VERSION/
 else
-  echo "The path does not contain a activemq distribution"
+    echo "The path does not contain a activemq distribution"
 fi
 
 echo "Server started!"
