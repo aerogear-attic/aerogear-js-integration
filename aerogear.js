946780c93270c5ac21baa88d7a75030f101e78e5
tolis-e <tolisemm@yahoo.gr>
2014-01-19 10:44:49 +0100

    keep API consistent - remove STOMP unsubscribe test after callback removal in src

--- aerogear.js
+++ aerogear.js
@@ -1,4 +1,4 @@
-/*! AeroGear JavaScript Library - v1.4.0-dev - 2014-01-17
+/*! AeroGear JavaScript Library - v1.4.0-dev - 2014-01-19
 * https://github.com/aerogear/aerogear-js
 * JBoss, Home of Professional Open Source
 * Copyright Red Hat, Inc., and individual contributors
@@ -5383,7 +5383,7 @@ AeroGear.Notifier.adapters.stompws.prototype.subscribe = function( channels, res
     @param {Function} [onData] - callback to be executed when data is sent and received
     @example
     // Log data being sent and received
-    notifier.clients.client2.debug({
+    notifier.clients.client2.debug(
         function(data) {
             console.log( data );
         }
@@ -5396,7 +5396,7 @@ AeroGear.Notifier.adapters.stompws.prototype.debug = function( onData ) {
                 onData.apply( this, arguments );
             }
         };
-    
+
     if ( client ) {
         client.debug = debug;
     }
@@ -5425,18 +5425,13 @@ AeroGear.Notifier.adapters.stompws.prototype.debug = function( onData ) {
     ]);
  */
 AeroGear.Notifier.adapters.stompws.prototype.unsubscribe = function( channels ) {
-    var index, i,
-        client = this.getClient(),
+    var client = this.getClient(),
         thisChannels = this.getChannels();
 
     channels = AeroGear.isArray( channels ) ? channels : [ channels ];
-    for ( i = 0; i < channels.length; i++ ) {
-        index = this.getChannelIndex( channels[ i ].address );
-        client.unsubscribe( thisChannels[ index ].id );
-        this.removeChannel( thisChannels[ index ] );
-        if ( channels[ i ].callback ) {
-            channels[ i ].callback.apply( this, arguments );
-        }
+    for ( var i = 0; i < channels.length; i++ ) {
+        client.unsubscribe( channels[ i ].id || thisChannels[ this.getChannelIndex( channels[ i ].address ) ].id );
+        this.removeChannel( channels[ i ] );
     }
 };
 
